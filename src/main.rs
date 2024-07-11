7ab7c89ecde6d19b6f616b8ccfeac5db6e44f7b3
iusami_hacarus <ippei@hacarus.com>
2024-07-11 10:03:45 +0900

    added basic logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use axum::{routing::get, Router};
+use std::env;
+use tracing_subscriber;
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new().route("/", get(root));
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {:?}", listener.local_addr());
 
     axum::serve(listener, app)
     .await
